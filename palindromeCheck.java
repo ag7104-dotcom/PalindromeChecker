34f984b4c0289b5306a084340820d915780d867c
adithG17 <adithg17@gmail.com>
2026-02-27 14:44:17 +0530

    UC3 - Palindrome Check Using String Reverse

--- palindromeCheck.java
+++ palindromeCheck.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class palindromeCheck{
 
     public static boolean isPalindrome(String str){
-        String reversedStr = new StringBuilder(str).reverse().toString();
-        return str.equals(reversedStr);
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
     public static void main(String[] args){
         System.out.println("Welcome to the Palindrome Checker Manager System!\nVersion 1.0\nSystem initialized successfully!\n");
