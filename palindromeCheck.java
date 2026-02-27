12be6815c7a049fefa12f2abacc2bf998cff98a5
adithG17 <adithg17@gmail.com>
2026-02-27 14:21:57 +0530

    UC1: Application Entry & Welcome Message

---
+++ palindromeCheck.java
@@ -0,0 +1,9 @@
+package PalindromeChecker;
+
+import java.util.Scanner;
+
+public class palindromeCheck{
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Manager System!\nVersion 1.0\nSystem initialized successfully!\n");
+    }
+}
