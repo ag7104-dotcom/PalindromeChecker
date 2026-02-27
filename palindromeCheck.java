24ffa63cce2accdba4fe2a41e3fe8728aacb31e2
adithG17 <adithg17@gmail.com>
2026-02-27 14:33:38 +0530

    UC2 - Print a Hardcoded Palindrome Result

--- palindromeCheck.java
+++ palindromeCheck.java
@@ -3,7 +3,18 @@ package PalindromeChecker;
 import java.util.Scanner;
 
 public class palindromeCheck{
+
+    public static boolean isPalindrome(String str){
+        String reversedStr = new StringBuilder(str).reverse().toString();
+        return str.equals(reversedStr);
+    }
     public static void main(String[] args){
         System.out.println("Welcome to the Palindrome Checker Manager System!\nVersion 1.0\nSystem initialized successfully!\n");
+        String str = "racecar";
+        if(isPalindrome(str)){
+            System.out.println("The string \"" + str + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + str + "\" is not a palindrome.");
+        }
     }
 }
